72be704575b55a6a5217aa717290c94affaa74bd
jawerty <jawerty210@gmail.com>
2014-02-14 20:46:26 -0500

    Added emulator

--- js/webnes.js
+++ js/webnes.js
@@ -1,1 +1,88 @@
-console.log('webnes')
+var iNES = function(nes) {
+  this.nes = nes;
+  this.audio = new webkitAudioContext();
+
+  // Initialize screen context
+  this.screen = document.getElementById('screen');
+  this.canvasContext = this.screen.getContext('2d');
+  this.canvasContext.fillStyle = 'black';
+  this.canvasContext.fillRect(0, 0, 256, 240);
+
+  // Initialize framebuffer
+  this.canvasData = this.canvasContext.getImageData(0, 0, 256, 240);
+  for (var i = 3; i < this.canvasData.data.length - 3; i += 4) {
+      this.canvasData.data[i] = 0xFF;
+  }
+
+  // Unlock audio and start
+  var self = this;
+  window.addEventListener('touchstart', function() {
+    var source = self.audio.createBufferSource();
+    source.buffer = self.audio.createBuffer(1, 1, 22050);
+    source.connect(self.audio.destination);
+    source.start(0);
+
+    self.nes.stop();
+    Dropbox.choose({
+        success: function(files) {
+          var xhr = new XMLHttpRequest();
+          xhr.overrideMimeType('text/plain; charset=x-user-defined');
+          xhr.open("GET", files[0].link, true);
+          xhr.onload = function (e) {
+            self.nes.loadRom(xhr.responseText);
+            self.nes.start();
+          };
+          xhr.send(null);
+        },
+        cancel: function() {
+
+        },
+        linkType: "direct",
+        multiselect: false,
+        extensions: ['.nes']
+    });
+  }, false);
+
+  var a = document.getElementById('controls');
+  var b = a.getContext('2d');
+  b.fillStyle = 'green';
+  b.fillRect(0, 0, 320, 240);
+};
+
+iNES.prototype = {
+  updateStatus: function(status) {
+    console.log('JSNES: ' + status);
+  },
+  writeFrame: function(buffer, prevBuffer) {
+    var data = this.canvasData.data;
+    for (var i = 0; i < 256 * 240; i++) {
+        var pixel = buffer[i];
+        if (pixel != prevBuffer[i]) {
+            var j = i * 4;
+            data[j] = pixel & 0xFF;
+            data[j + 1] = (pixel >> 8) & 0xFF;
+            data[j + 2] = (pixel >> 16) & 0xFF;
+            prevBuffer[i] = pixel;
+        }
+    }
+    this.canvasContext.putImageData(this.canvasData, 0, 0);
+  },
+  writeAudio: function(leftSamples, rightSamples) {
+  	var buffer = this.audio.createBuffer(2, leftSamples.length, this.nes.papu.sampleRate);
+  	buffer.getChannelData(0).set(leftSamples);
+    buffer.getChannelData(1).set(rightSamples);
+    var source = this.audio.createBufferSource();
+    source.buffer = buffer;
+    source.connect(this.audio.destination);
+    source.start(0);
+  }
+};
+
+// Prevent scrolling
+function stopScrolling(touchEvent) { touchEvent.preventDefault(); }
+document.addEventListener('touchmove', stopScrolling, false);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var nes = new JSNES({ 'ui': iNES, fpsInterval: 2000, emulateSound: true });
+  console.log("webnes initialized");
+});
